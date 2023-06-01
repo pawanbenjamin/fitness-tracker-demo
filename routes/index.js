f320e3f430d36d18b09a5afa34c6a97934b81d11
Pawan Benjamin <pawanbenjaminfsa@Pawans-MBP.lan>
2023-06-01 08:18:57 -0500

    added client react app

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 // GET /api/health
 router.get("/health", (req, res, next) => {
-  res.send("API is healthy!");
+  res.send({
+    message: "Api is up and healthy!",
+  });
 });
 
 router.use("/users", require("./users"));
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -14,12 +15,17 @@ server.use(express.json());
 server.use(morgan("dev"));
 server.use(cors());
 server.use(cookieParser(process.env.COOKIE_SECRET));
+server.use(express.static(path.join(__dirname, "./client", "dist")));
 
 // Routes
 server.use("/api", require("./routes"));
 
 server.get("/test", authRequired, (req, res, next) => {
   res.send("You are authorized!");
+});
+
+server.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
 });
 
 server.use((err, req, res, next) => {
