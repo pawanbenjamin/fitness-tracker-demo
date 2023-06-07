8320e4fbd4dff92194a441caf423402064f2c08f
pawanbenjamin <pawanbenjamin@gmail.com>
2023-06-06 22:59:26 -0500

    cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,12 @@
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import AuthForm from "./components/auth/AuthForm";
-
-import "./App.css";
 import Nav from "./components/Nav";
+import "./App.css";
 
 function App() {
-  const [healthMessage, setHealthMessage] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function checkAPIHealth() {
-      try {
-        const response = await fetch("/api/health");
-        const result = await response.json();
-        setHealthMessage(result.message);
-      } catch (error) {
-        setError(error);
-      }
-    }
-    checkAPIHealth();
-  }, []);
-
   return (
     <>
-      {error && <p>{JSON.stringify(error, null, 2)}</p>}
       <Nav />
-      {healthMessage && <p>{healthMessage}</p>}
       <Routes>
         <Route path="/" element={<h2>Home</h2>} />
         <Route path="/register" element={<AuthForm />} />
